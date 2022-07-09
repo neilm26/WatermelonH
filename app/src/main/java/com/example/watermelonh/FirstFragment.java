5abb7e400e5458fe0467f63a6c91aaf5d8421164
neilm26 <neilm26@educbe.ca>
2022-07-08 19:22:47 -0600

    testing environment

--- app/src/main/java/com/example/watermelonh/FirstFragment.java
+++ app/src/main/java/com/example/watermelonh/FirstFragment.java
@@ -2,10 +2,7 @@ package com.example.watermelonh;
 
 import static android.app.Activity.RESULT_OK;
 
-import static com.example.watermelonh.MainActivity.contentUri;
-import static com.example.watermelonh.MainActivity.data;
 import static com.example.watermelonh.MainActivity.imageDir;
-import static com.example.watermelonh.MainActivity.saveToFile;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -40,6 +37,7 @@ import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.watermelonh.databinding.FragmentFirstBinding;
@@ -49,23 +47,26 @@ import org.jetbrains.annotations.Nullable;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 public class FirstFragment extends Fragment  {
 
     private FragmentFirstBinding binding;
+    private ImageView sideOfWatermelon;
+
+    private Bitmap savedBitmap = null;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -74,18 +75,10 @@ public class FirstFragment extends Fragment  {
         super.onViewCreated(fragmentView, savedInstanceState);
         TextView content = (TextView) fragmentView.findViewById(R.id.pytorch);
         ImageView imageView = (ImageView) fragmentView.findViewById(R.id.keyboard);
-        ImageView sideOfWatermelon = (ImageView) fragmentView.findViewById(R.id.mouse);
+        sideOfWatermelon = (ImageView) fragmentView.findViewById(R.id.mouse);
+
 
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-
-        if (getArguments() != null) {
-            String text = getArguments().getString("className", "No Input Given");
-            byte[] byteArray = getArguments().getByteArray("classByteArray");
-            Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-
-            content.setText(text);
-            imageView.setImageBitmap(bitmap);
-        }
 
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,7 +95,29 @@ public class FirstFragment extends Fragment  {
                 if(result.getResultCode() == RESULT_OK && result.getData() != null){
                     Bundle bundle = result.getData().getExtras();
                     Bitmap bitmap = (Bitmap) bundle.get("data");
-                    sideOfWatermelon.setImageBitmap(bitmap);
+
+                    try {
+                        File tempFile = saveToFile();
+
+                        Log.e("file path",currentPhotoPath);
+
+                        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG,90,outStream);
+
+                        byte[] bitmapData = outStream.toByteArray();
+                        FileOutputStream fileOutStream = new FileOutputStream(tempFile);
+
+                        fileOutStream.write(bitmapData);
+                        fileOutStream.flush();
+                        fileOutStream.close();
+
+                        Log.e("Working","yesss");
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    savedBitmap = bitmap;
                 }
             }
         });
@@ -117,10 +132,42 @@ public class FirstFragment extends Fragment  {
         binding.upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if (getArguments() != null) {
+                    String text = getArguments().getString("className", "No Input Given");
+                    byte[] byteArray = getArguments().getByteArray("classByteArray");
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+                    content.setText(text);
+                    imageView.setImageBitmap(bitmap);
+                }
                 //open folder containing images
-                startActivity(data);
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//
+//                //intent.setDataAndType(Uri.parse("/storage/emulated/0/Android/data/com.example.watermelonh/files/Watermelon_Images/"), "resource/folder");
+//                startActivity(intent);
             }
         });
+
+        binding.revert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageDrawable(null);
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sideOfWatermelon.setImageDrawable(null);
+
+        if (savedBitmap!=null) {
+            sideOfWatermelon.setImageBitmap(savedBitmap);
+            Log.d("Resuming","Loading new image");
+
+        }
     }
 
     @Override
@@ -133,6 +180,17 @@ public class FirstFragment extends Fragment  {
         startActivity(intent);
     }
 
+    String currentPhotoPath;
+
+    private File saveToFile() throws IOException {
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timestamp + "_" + ".jpg";
+
+        File image = new File(imageDir,imageFileName);
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     public static FirstFragment newInstance(String text, Bitmap bitmap) {
         FirstFragment firstFragment = new FirstFragment();
