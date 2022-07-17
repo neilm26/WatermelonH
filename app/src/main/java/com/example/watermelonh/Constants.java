7484be0f4a7d0e0f081547f74148b6632ddf662d
neil ma <neilm26@educbe.ca>
2022-07-16 19:42:25 -0600

    huge changes

--- app/src/main/java/com/example/watermelonh/Constants.java
+++ app/src/main/java/com/example/watermelonh/Constants.java
@@ -13,8 +13,12 @@ public class Constants {
 
     public static String result;
     public static String resultFront;
+    public static int frontQuality;
+    public static int sideQuality;
+    public static String notWatermelon = "Not a watermelon, please try again";
     public static Bitmap imageBitmap; //gotta rename it
     public static Bitmap imageBitmapFront;
     public static File imageDir;
     public static Module module;
+    public static Module watermelonModule;
 }
